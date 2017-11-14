6366653e0b7bbc3d4dc8e1047f35636d68284d89
Arne Huang <ahuang@hubba.com>
2017-11-14 10:52:27 -0500

    test

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 import ssl
 import sys
 
-PORT = int((sys.argv[1] or 4333))
+# https://blog.didierstevens.com/2015/03/30/howto-make-your-own-cert-with-openssl-on-windows/
+# openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes
 
-httpd = HTTPServer(('localhost', PORT), SimpleHTTPRequestHandler)
+try:
+    PORT = int(sys.argv[1])
+except:
+    PORT = 4443
 
-httpd.socket = ssl.wrap_socket(httpd.socket,
-                               certfile='server.pem', server_side=True)
-print('running')
+httpd = HTTPServer(('localhost', 4443), SimpleHTTPRequestHandler)
+
+httpd.socket = ssl.wrap_socket(httpd.socket,certfile='server.pem', server_side=True)
+print('running port: ' + str(PORT))
 httpd.serve_forever()
